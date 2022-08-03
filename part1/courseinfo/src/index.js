b9325aa3187e8d823811f700581fa740d762a306
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2022-08-02 22:15:51 -0400

    finished 1.1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = document.getElementById('root')
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
