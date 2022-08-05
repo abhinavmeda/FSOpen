63bc32f486a9b0eb95554f986350404675e01469
Abhinav Meda <abhinavmeda1@gmail.com>
2022-08-04 22:17:28 -0400

    finished 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,31 +19,37 @@ const Data = ({type, data}) => {
     </div>
   )
 }
-const Statistics = ({good, bad, neutral}) => {
+const Statistics = ({ good, bad, neutral, all, average, positive }) => {
   return(
     <div>
       <Data type={'good'} data={good}/>
-      <Data type={'neutral'} data={bad}/>
-      <Data type={'bad'} data={neutral}/>
+      <Data type={'neutral'} data={neutral}/>
+      <Data type={'bad'} data={bad}/>
+      <Data type={'all'} data={all}/>
+      <Data type={'average'} data={average}/>
+      <Data type={'positive'} data={positive}/>
     </div>
     
   )
 }
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  let average = (good - bad) / all
+  let positive = (good / all) * 100
+
+  if (isNaN(average)){average = 0}
+  if(isNaN(positive)){positive = 0}
   
-  const incrementGood = () => {
-    setGood(good + 1)
-  }
-  const incrementNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const incrementBad = () => {
-    setBad( bad + 1)
-  }
+  const incrementGood = () => setGood(good + 1)
+  
+  const incrementNeutral = () => setNeutral(neutral + 1)
+
+  const incrementBad = () => setBad( bad + 1)
+
   return (
     <div>
       <Heading title={"give feedback"}/>
@@ -51,7 +57,7 @@ const App = () => {
       <Button name={"neutral"} onClick={incrementNeutral}/>
       <Button name={"bad"} onClick={incrementBad}/>
       <Heading title={"statistics"}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive + ' %'}/>
     </div>
     
   );
