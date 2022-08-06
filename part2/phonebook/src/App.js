2fb38f70e400059fb49e1ec6b93b2d8edb63df7e
Abhinav Meda <abhinavmeda1@gmail.com>
2022-08-06 17:17:44 -0400

    Finished 2.6 and refactored the starter code by creating components for the headers, form, and display. I also moved the state which stored user input from the App component to the form component because it manages user input and so the state should be defined there

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,56 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+const Heading = ({ heading }) => <h2>{heading}</h2>
 
+const Display = ({ registrants }) => {
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
+      {registrants.map((person, i) => <div key={i}>{person.name}</div>)}
+    </div>
+  )
+}
+
+const Form = ({registrants, setRegistrants}) => {
+  
+  const [formNameInput, setNameInput] = useState('')
+
+  const addRegistrants = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: formNameInput
+    }
+    
+    setRegistrants(registrants.concat(newPerson))
+    setNameInput('')
+  }
+
+  const handleInputState = (event) => setNameInput(event.target.value)
+  
+  return (
+    <>
+    <form onSubmit={addRegistrants}>
         <div>
-          name: <input />
+          name: <input value={formNameInput} onChange={handleInputState}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  return (
+    <div>
+      <Heading heading={"Phonebook"}/>
+      <Form registrants={persons} setRegistrants={setPersons}/>
+      <Heading heading={"Numbers"}/>
+      <Display registrants={persons}/>
     </div>
   )
 }
