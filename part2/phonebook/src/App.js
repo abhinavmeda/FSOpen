802c0bc8e409c7b8d4da8d92fbbb36680828ea8c
Abhinav Meda <abhinavmeda1@gmail.com>
2022-08-06 23:06:35 -0400

    finished 2.8 and 2.9*

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,41 +5,67 @@ const Heading = ({ heading }) => <h2>{heading}</h2>
 const Display = ({ registrants }) => {
   return (
     <div>
-      {registrants.map((person, i) => <div key={i}>{person.name}</div>)}
+      {registrants.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
     </div>
   )
 }
 
+const Filter = ({ filter, set }) => {
+  const handler = (event) => {
+    set(event.target.value)
+  }
+  return (
+    <div>
+      filter by <input value={filter} onChange={handler}/>
+    </div>
+  )
+  
+}
 const Form = ({registrants, setRegistrants}) => {
   
   const [formNameInput, setNameInput] = useState('')
+  const [formNumberInput, setNumberInput] = useState('')
 
   const addRegistrants = (event) => {
     
     event.preventDefault()
     
-    const exists = () => registrants.some(person => person.name === formNameInput)
+    const exists = () => registrants.some(person => person.name === formNameInput || person.number === formNumberInput)
     
-    if(exists()){
-      alert(`${formNameInput} already exists in the phonebook`)
+    if(formNameInput === '' || formNumberInput === ''){
+      alert(`Need both name and phone number`)
       setNameInput('')
+      setNumberInput('')
     }
     else{
-      const newPerson = {
-        name: formNameInput
+      if(exists()){
+        alert(`${formNameInput} or ${formNumberInput} already exists in the phonebook`)
+        setNameInput('')
+        setNumberInput('')
       }
-      setRegistrants(registrants.concat(newPerson))
-      setNameInput('')
-    }
+      else{
+        const newPerson = {
+          name: formNameInput,
+          number: formNumberInput
+        }
+        setRegistrants(registrants.concat(newPerson))
+        setNameInput('')
+        setNumberInput('')
+      }
+    } 
   }
 
-  const handleInputState = (event) => setNameInput(event.target.value)
-  
+  const handleNameInputState = (event) => setNameInput(event.target.value)
+  const handleNumberInputState = (event) => setNumberInput(event.target.value)
+
   return (
     <>
     <form onSubmit={addRegistrants}>
         <div>
-          name: <input value={formNameInput} onChange={handleInputState}/>
+          name: <input value={formNameInput} onChange={handleNameInputState}/>
+        </div>
+        <div>
+          number: <input value={formNumberInput} onChange={handleNumberInputState}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -51,15 +77,21 @@ const Form = ({registrants, setRegistrants}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
+  const [filter, setFilter] = useState('')
   return (
     <div>
       <Heading heading={"Phonebook"}/>
+      <Filter filter={filter} set={setFilter}/>
+      <Heading heading={"add a new"}/>
       <Form registrants={persons} setRegistrants={setPersons}/>
       <Heading heading={"Numbers"}/>
-      <Display registrants={persons}/>
+      <Display registrants={filter === '' ? persons : persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))}/>
     </div>
   )
 }
